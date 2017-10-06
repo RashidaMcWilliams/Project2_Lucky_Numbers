1bc67f30078292cfbd92ff7b4a6bddb2e1d7b809
McShi <rashida.mcwilliams@gmail.com>
2017-10-06 10:30:05 -0400

    Initial commit

---
+++ Project2_Lucky_Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_Lucky_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random rando = new Random();
+            //Inclusive of first number but exclusive of second number
+            //Console.WriteLine(rando.Next(5, 15));
+
+            //int firstNum = rando.Next(5, 15);
+            //int secondNum = rando.Next(5, 15);
+
+            //while (firstNum == secondNum)
+            //{
+            //    secondNum = rando.Next(5, 15);
+            }
+        }
+    }
+}
